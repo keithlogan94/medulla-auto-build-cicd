447a57b798405e2c8321c1e52f60ad8eb5e6ccb9
Keith Becker <keith.becker@keysight.com>
2021-08-22 11:04:00 -0600

    update

--- check-build/check-build.sh
+++ check-build/check-build.sh
@@ -10,8 +10,8 @@ fi
 
 while true
 do
-  echo "waiting 60 seconds";
-  sleep 60
+  echo "waiting 300 seconds";
+  sleep 300
   echo "checking if system needs to rebuild docker image";
   COMMIT_HASH=$(git ls-remote https://github.com/recro/medulla-frontend.git HEAD | awk '{ print $1}')
   echo "$COMMIT_HASH";
@@ -22,16 +22,17 @@ do
     echo "It's there."
   else
     echo "local repository does not contain the latest hash so rebuilding";
-    git --git-dir=./medulla-frontend/.git fetch
-    git --git-dir=./medulla-frontend/.git reset --hard origin/main
-    echo "building docker file";
-    cd "$(dirname "$0")" || exit 1
-    cd ./medulla-frontend/frontend/ || exit 1
-    docker rmi recrodeveloper/medulla-frontend:latest
-    docker build -t recrodeveloper/medulla-frontend:latest .
-    cd ../../
-    docker push recrodeveloper/medulla-frontend:latest
-    docker stop medulla
-    docker run -d --rm -p 80:5000 --name=medulla recrodeveloper/medulla-frontend:latest
+    /app/build.sh
+#    git --git-dir=./medulla-frontend/.git fetch
+#    git --git-dir=./medulla-frontend/.git reset --hard origin/main
+#    echo "building docker file";
+#    cd "$(dirname "$0")" || exit 1
+#    cd ./medulla-frontend/frontend/ || exit 1
+#    docker rmi recrodeveloper/medulla-frontend:latest
+#    docker build -t recrodeveloper/medulla-frontend:latest .
+#    cd ../../
+#    docker push recrodeveloper/medulla-frontend:latest
+#    docker stop medulla
+#    docker run -d --rm -p 80:5000 --name=medulla recrodeveloper/medulla-frontend:latest
   fi
 done
