5a92c380cde5e854a1b17ca25a36e001545ea9e4
Keith Becker <keith.becker@keysight.com>
2021-08-22 13:06:12 -0600

    update

--- src/build.sh
+++ src/build.sh
@@ -1,18 +1,7 @@
 
 
-echo "local repository does not contain the latest hash so rebuilding";
-git --git-dir=./medulla-frontend/.git fetch
-git --git-dir=./medulla-frontend/.git reset --hard origin/main
-echo "building docker file";
-cd "$(dirname "$0")" || exit 1
-cd ./medulla-frontend/frontend/ || exit 1
-docker rmi recrodeveloper/medulla-frontend:latest
-docker build -t recrodeveloper/medulla-frontend:latest .
-cd ../../
-docker push recrodeveloper/medulla-frontend:latest
+docker exec build-image /bin/sh /app/build.sh
+docker pull recrodeveloper/medulla-frontend:latest
 docker stop medulla
 docker run -d --rm -p 80:5000 --name=medulla recrodeveloper/medulla-frontend:latest
-
-
-
 
