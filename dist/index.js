cdb40c34b116dc341f2288dd277df5f35e832e9d
Keith Becker <keith.becker@keysight.com>
2021-08-22 13:57:17 -0600

    update

--- dist/index.js
+++ dist/index.js
@@ -23,44 +23,14 @@ app.get("/build", function (req, res) {
   }
 
   if (accessKey === MATCH_ACCESS_KEY) {
-    exec('docker exec build-image /bin/sh /app/build.sh', function (error1, stdout1, stderr1) {
-      if (error1) {
-        console.error("exec error: ".concat(error1));
+    exec('./src/build.sh', function (error2, stdout2, stderr2) {
+      if (error2) {
+        console.error("exec error: ".concat(error2));
         return;
-      } else {
-        exec('docker pull recrodeveloper/medulla-frontend:latest', function (error2, stdout2, stderr2) {
-          if (error2) {
-            console.error("exec error: ".concat(error2));
-            return;
-          } else {
-            exec('docker stop medulla', function (error3, stdout3, stderr3) {
-              if (error3) {
-                console.error("exec error: ".concat(error3));
-                return;
-              } else {
-                exec('docker run -d --rm -p 80:5000 --name=medulla recrodeveloper/medulla-frontend:latest', function (error4, stdout4, stderr4) {
-                  if (error4) {
-                    console.error("exec error: ".concat(error4));
-                    return;
-                  } else {}
+      } else {}
 
-                  console.log("stdout: ".concat(stdout4));
-                  console.error("stderr: ".concat(stderr4));
-                });
-              }
-
-              console.log("stdout: ".concat(stdout3));
-              console.error("stderr: ".concat(stderr3));
-            });
-          }
-
-          console.log("stdout: ".concat(stdout2));
-          console.error("stderr: ".concat(stderr2));
-        });
-      }
-
-      console.log("stdout: ".concat(stdout1));
-      console.error("stderr: ".concat(stderr1));
+      console.log("stdout: ".concat(stdout2));
+      console.error("stderr: ".concat(stderr2));
     });
     res.send("Success");
   } else {
--- src/index.js
+++ src/index.js
@@ -13,42 +13,15 @@ app.get("/build", (req, res) => {
         return res.send("Failed to build match access key was not set");
     }
     if (accessKey === MATCH_ACCESS_KEY) {
-        exec('docker exec build-image /bin/sh /app/build.sh', (error1, stdout1, stderr1) => {
-            if (error1) {
-                console.error(`exec error: ${error1}`);
+        exec('./src/build.sh', (error2, stdout2, stderr2) => {
+            if (error2) {
+                console.error(`exec error: ${error2}`);
                 return;
             } else {
-                exec('docker pull recrodeveloper/medulla-frontend:latest', (error2, stdout2, stderr2) => {
-                    if (error2) {
-                        console.error(`exec error: ${error2}`);
-                        return;
-                    } else {
-                        exec('docker stop medulla', (error3, stdout3, stderr3) => {
-                            if (error3) {
-                                console.error(`exec error: ${error3}`);
-                                return;
-                            } else {
-                                exec('docker run -d --rm -p 80:5000 --name=medulla recrodeveloper/medulla-frontend:latest', (error4, stdout4, stderr4) => {
-                                    if (error4) {
-                                        console.error(`exec error: ${error4}`);
-                                        return;
-                                    } else {
 
-                                    }
-                                    console.log(`stdout: ${stdout4}`);
-                                    console.error(`stderr: ${stderr4}`);
-                                });
-                            }
-                            console.log(`stdout: ${stdout3}`);
-                            console.error(`stderr: ${stderr3}`);
-                        });
-                    }
-                    console.log(`stdout: ${stdout2}`);
-                    console.error(`stderr: ${stderr2}`);
-                });
             }
-            console.log(`stdout: ${stdout1}`);
-            console.error(`stderr: ${stderr1}`);
+            console.log(`stdout: ${stdout2}`);
+            console.error(`stderr: ${stderr2}`);
         });
         res.send("Success")
     } else {
